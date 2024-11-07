a1934aa3c80d74a233b50d6b40588696cd49bf1a
srfj <122041136+srfj@users.noreply.github.com>
2024-11-07 23:47:14 +0800

    Add files via upload

---
+++ diy-part1.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#
+# https://github.com/P3TERX/Actions-OpenWrt
+# File name: diy-part1.sh
+# Description: OpenWrt DIY script part 1 (Before Update feeds)
+#
+# Copyright (c) 2019-2024 P3TERX <https://p3terx.com>
+#
+# This is free software, licensed under the MIT License.
+# See /LICENSE for more information.
+#
+
+# 添加软件包
+git clone https://github.com/Dboykey/luci-app-usb-printer package/luci-app-usb-printer
