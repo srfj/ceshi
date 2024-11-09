cfe291d7ce7a247f73e7afe48acee1d2ba334bb2
srfj <122041136+srfj@users.noreply.github.com>
2024-11-09 10:00:32 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -11,6 +11,9 @@
 #
 
 # Modify default IP
+sed -i 's/192.168.1.1/192.168.0.1/g' package/base-files/luci2/bin/config_generate
+
+# Modify default IP
 sed -i 's/192.168.1.1/192.168.0.1/g' package/base-files/files/bin/config_generate
 
 # Modify default theme
