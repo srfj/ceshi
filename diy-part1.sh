f3b91b1037a2353376d63b4b122207c2da46aac7
srfj <122041136+srfj@users.noreply.github.com>
2024-11-11 16:10:21 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -10,6 +10,9 @@
 # See /LICENSE for more information.
 #
 
+# 移除要替换的包
+rm -rf feeds/luci/themes/luci-theme-argon
+
 # 添加软件包
 git clone https://github.com/srfj/luci-app-mwan3helper-chinaroute package/luci-app-mwan3helper-chinaroute
-#git clone https://github.com/jerrykuku/luci-theme-argon package/luci-theme-argon
+git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon package/luci-theme-argon
